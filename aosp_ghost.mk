1f1d23563d6728d81c94986855320ff7df01c25c
Omkar <djomi400@gmail.com>
2016-10-17 07:41:39 +0000

    ghost: This is AOSP!

---
+++ aosp_ghost.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/motorola/ghost/full_ghost.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+PRODUCT_RELEASE_NAME := MOTO X
+PRODUCT_NAME := aosp_ghost
